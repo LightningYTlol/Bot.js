daaea0bdc530712447ddc039ad97731cd6e41260
LightningYTlol <73615108+LightningYTlol@users.noreply.github.com>
2020-10-29 16:17:02 -0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const { Client } = require('discord.js')
+const client = new Client();
+const PREFIX = "*";
+
+client.on('ready', () => {
+    console.log(`${client.user.tag} has logged in`);
+});
+
+client.on('message', (message) =>{
+    console.log(`[${message.author.tag}] sent: ${message.content}`);
+    if (message.content.startsWith(PREFIX)) {
+        const [CMD_NAME, ...args] = message.content
+            .trim()
+            .substring(PREFIX.length)
+            .split(" ");
+        console.log(CMD_NAME);
+        console.log(args);
+    }
+});
+
+client.login(process.env.DISCORDJS_BOT_TOKEN)
+
